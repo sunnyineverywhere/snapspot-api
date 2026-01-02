76c9de025173b04df750d909e407903e76141d68
sunnyineverywhere <sunnyinsummer@ewhain.net>
2026-01-03 00:43:07 +0900

    fix(#6): HealthCheckController 주석 수정

--- snapspot-backend/src/main/kotlin/xyz/snapspot/controller/HealthCheckController.kt
+++ snapspot-backend/src/main/kotlin/xyz/snapspot/controller/HealthCheckController.kt
@@ -1,11 +1,12 @@
-// Health Check Controller in Kotlin
-
 package xyz.snapspot.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Health Check Controller
+ */
 @RestController
 @RequestMapping("/health-check")
 class HealthCheckController {
